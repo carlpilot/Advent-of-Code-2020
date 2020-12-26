dc78a997308a82d706b6ab2450ee57c30a8b4857
Carl B <carlbert101@gmail.com>
2020-12-26 15:23:22 +0000

    Day1 part2

--- Day1.py
+++ Day1.py
@@ -8,12 +8,19 @@ Created on Sat Dec 26 15:14:25 2020
 expenses = open("Day1-input.txt", "r").read().split("\n")
 expenses = expenses[0:len(expenses)-2] # remove last blank value
 
-def run ():
+def part1 ():
+    # 2 numbers sum to 2020. What is their product?
     for i in expenses:
         for j in expenses:
             if(int(i) + int(j) == 2020):
-                print(int(i) * int(j))
-                return
+                return int(i) * int(j)
     print("No matches")
 
-run()
+def part2 ():
+    # 3 numbers sum to 2020. What is their product?
+    for i in expenses:
+        for j in expenses:
+            for k in expenses:
+                if(int(i) + int(j) + int(k) == 2020):
+                    return int(i) * int(j) * int(k)
+    print("No matches")
